29dc4813406d9a6802a2c7d53c1371c88fc54366
gabriel-alves-p <gabrielmpalves@gmail.com>
2021-08-22 10:39:26 +0000

    create a class to define the gameboard and index the empty spaces for available moves

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class NoughtsNCrosses:
+    """
+    create a game board for the game
+    """
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]  # 3x3 gameboard
+        self.current_winner = None  # track the winner
+    
+    def print_board(self):
+        # gets the rows
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
+    
+    @staticmethod
+    def print_board_nums():
+        # tells us which number corresponds to each empty space
+        # i.e. 1 | 2 | 3 etc.
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]  # noqa
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+
+
