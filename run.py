ad3ea6c4bb69aa1362689d5a1647a2783cea574b
gabriel-alves-p <gabrielmpalves@gmail.com>
2021-08-24 06:50:20 +0000

    fix diagonal winning move on lines 74-82 run.py

--- run.py
+++ run.py
@@ -42,19 +42,19 @@ class NoughtsAndCrosses:
     def num_empty_squares(self):
         return self.board.count(' ')
 
-    def make_move(self, square, letter):
+    def make_move(self, square, tag):
         """
         if the move is valid, proceed & return True
         if the move is invalid, returns False
         """
         if self.board[square] == ' ':
-            self.board[square] = letter
-            if self.winner(square, letter):
-                self.current_winner = letter
+            self.board[square] = tag
+            if self.winner(square, tag):
+                self.current_winner = tag
             return True
         return False
 
-    def winner(self, square, letter):
+    def winner(self, square, tag):
         """
         winner if player ticks 3 in a row (either vertically, horizontally, or diagonally) # noqa
         """
@@ -62,23 +62,23 @@ class NoughtsAndCrosses:
         # checks horizontally
         row_ind = square // 3
         row = self.board[row_ind*3: (row_ind + 1) * 3]
-        if all([spot == letter for spot in row]):
+        if all([spot == tag for spot in row]):
             return True
 
         # checks vertically
         col_ind = square % 3
         column = [self.board[col_ind+i*3] for i in range(3)]
-        if all([spot == letter for spot in column]):
+        if all([spot == tag for spot in column]):
             return True
 
         # checks diagonally
         # this is only possible if all squares are even numbers
         if square % 2 == 0:
             diagonal1 = [self.board[i] for i in [0, 4, 8]]
-            if all([spot == letter for spot in diagonal1]):
+            if all([spot == tag for spot in diagonal1]):
                 return True
             diagonal2 = [self.board[i] for i in [2, 4, 6]]
-            if all([spot == letter for spot in diagonal2]):
+            if all([spot == tag for spot in diagonal2]):
                 return True
 
         # if all above fails
