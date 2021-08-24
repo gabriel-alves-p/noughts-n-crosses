f1fe7ac57e4093755c540c9c7a0b4b1d230467f6
gabriel-alves-p <gabrielmpalves@gmail.com>
2021-08-24 06:46:52 +0000

    fix equation to make the board work properly on line 20

--- players.py
+++ players.py
@@ -1,53 +1,47 @@
-# import libraries to be used within the player classes
 import random
-import math
 
 
-class BasePlayer:
+class Player:
     """
     define a class for the user as the player
     """
-    def __init__(self, tag):
-        # tag stands for cross or nought
-        self.tag = tag
+    def __init__(self, letter):
+        self.letter = letter
 
     def get_move(self, game):
         pass
 
 
-class Computer(BasePlayer):
+class RandomComputerPlayer(Player):
     """
     random computer player class, built on top of BasePlayer superclass
     """
-
-    def __init__(self, tag):
-        super().__init__(tag)
+    def __init__(self, letter):
+        super().__init__(letter)
 
     def get_move(self, game):
-        # chooses a random available spot for their next move
         square = random.choice(game.available_moves())
+        return square
 
 
-class UserPlayer(BasePlayer):
+class HumanPlayer(Player):
     """
     player class for the user to play as, built on top of BasePlayer superclass
     """
-
-    def __init__(self, tag):
-        super().__init__(tag)
+    def __init__(self, letter):
+        super().__init__(letter)
 
     def get_move(self, game):
         valid_square = False
-        value = None
+        val = None
         while not valid_square:
-            square = input(self.tag + '\'s turn. Input move (0-8):')
-            # checking whether value is an integer ensures the user has input a valid number # noqa
-            # also checks whether the spot is available or not
+            square = input(self.letter + '\'s turn. Input move (0-8):')
             try:
-                value = int(square)
-                if value not in game.available_moves():
+                val = int(square)
+                if val not in game.available_moves():
                     raise ValueError
                 valid_square = True
             except ValueError:
                 print('Invalid square. Try again.')
-        return value
+
+        return val
--- run.py
+++ run.py
@@ -1,30 +1,136 @@
-class NoughtsNCrosses:
+from players import UserPlayer, ComputerPlayer
+import time
+
+
+class NoughtsAndCrosses:
     """
-    create a game board for the game
+    create a game-board for the game to run on
+    checks for available and invalid moves
+    counts empty squares
+    defines how to make moves
+    defines winning moves
     """
     def __init__(self):
-        self.board = [' ' for _ in range(9)]  # 3x3 gameboard
+        self.board = [' ' for _ in range(9)]  # 3x3 game-board
         self.current_winner = None  # track the winner
-    
+
     def print_board(self):
         # gets the rows
         for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
             print('| ' + ' | '.join(row) + ' |')
-    
+
     @staticmethod
     def print_board_nums():
-        # tells us which number corresponds to each empty space
-        # i.e. 1 | 2 | 3 etc.
+        """
+        tells us which number corresponds to each empty space
+        i.e. 1 | 2 | 3 etc.
+        """
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]  # noqa
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
 
+    def available_moves(self):
+        moves = []
+        for (i, spot) in enumerate(self.board):
+            if spot == ' ':
+                moves.append(i)
+        return moves
 
-def available_moves(self):
-    # return []
-    moves = []
-    for (i, spot) in enumerate(self.board):
-        if spot == ' ':
-            moves.append(i)
-    return moves
+    def empty_squares(self):
+        return ' ' in self.board
 
+    def num_empty_squares(self):
+        return self.board.count(' ')
+
+    def make_move(self, square, letter):
+        """
+        if the move is valid, proceed & return True
+        if the move is invalid, returns False
+        """
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
+            return True
+        return False
+
+    def winner(self, square, letter):
+        """
+        winner if player ticks 3 in a row (either vertically, horizontally, or diagonally) # noqa
+        """
+
+        # checks horizontally
+        row_ind = square // 3
+        row = self.board[row_ind*3: (row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+
+        # checks vertically
+        col_ind = square % 3
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+
+        # checks diagonally
+        # this is only possible if all squares are even numbers
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            if all([spot == letter for spot in diagonal2]):
+                return True
+
+        # if all above fails
+        return False
+
+
+def play(game, x_player, o_player, print_game=True):
+    """
+    returns the winner of the game or None if it's a tie
+    """
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X'  # starting tag
+
+    while game.empty_squares():
+        """
+        iterate as long as the game-board still has empty squares
+        gets move from appropriate player
+        """
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter + f' makes a move to square {square}')
+                game.print_board()
+                print('')  # empty line
+
+            if game.current_winner:
+                if print_game:
+                    print(letter + ' wins!')
+                return letter
+
+            # alternates / switches players for each round
+            if letter == 'X':
+                letter = 'O'
+            else:
+                letter = 'X'
+
+        time.sleep(3)
+    if print_game:
+        print('It\'s a tie!')
+
+
+def main():
+    x_player = HumanPlayer('X')
+    o_player = RandomComputerPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
+
+
+main()
